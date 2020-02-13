962279ff1302f23323c00d44b11b91914b30e431
Naman <namanthacker1965@gmail.com>
2020-02-13 15:30:17 +0530

    navbar modification

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,27 +10,29 @@ class NavBar extends React.Component {
 		let isLoggedIn = Object.entries(this.props.uData).length > 0 ? true : false;
 
 		return (
-				<nav className="navbar navbar-expand-md bg-dark navbar-dark">
-					<div className="navbar-header">
-						<Link className="navbar-brand" to="/">Code Sharer</Link>
-					</div>
-					<ul className="navbar-nav">
+			<nav className="navbar navbar-expand-md bg-dark navbar-dark">
+				<div className="navbar-header">
+					<Link className="navbar-brand" to="/">Code Sharer</Link>
+				</div>
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link className="nav-link" to="/submitsnippet">Submit-A-Snippet</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link" to="/searchsnippet">Search-A-Snippet</Link>
+					</li>
+				</ul>
+				<ul className="navbar-nav ml-auto">
+					{!isLoggedIn ? <React.Fragment><li className="nav-item ml-auto">
+						<Link className="nav-link" to="/login">Log in</Link>
+					</li>
 						<li className="nav-item">
-							<Link className="nav-link" to="/submitsnippet">Submit-A-Snippet</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/searchsnippet">Search-A-Snippet</Link>
-						</li>
-						{!isLoggedIn ? <React.Fragment><li className="nav-item ml-auto">
-							<Link className="nav-link" to="/login">Log in</Link>
-						</li>
-							<li className="nav-item ml-auto">
-								<Link className="nav-link" to="/signup">Sign up</Link>
-							</li></React.Fragment> :
-							<li className="nav-item ml-auto">Hellow, {this.props.uData.name}</li>}
+							<Link className="nav-link" to="/signup">Sign up</Link>
+						</li></React.Fragment> :
+						<li className="nav-item text-light">Hellow, {this.props.uData.name}</li>}
 
-					</ul>
-				</nav>
+				</ul>
+			</nav>
 		)
 	}
 }
